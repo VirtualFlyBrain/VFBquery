ca59be16f99d5dceb5fa7bfcee16e4dda467086c
Rob Court <rcourt@ed.ac.uk>
2022-11-04 13:47:36 +0000

    adding basic meta return

--- src/vfb_queries.py
+++ src/vfb_queries.py
@@ -1,4 +1,5 @@
-
+import pysolr
+from src.term_info_queries import deserialize_term_info
 
 def get_term_info(short_form: str):
     """
@@ -7,4 +8,27 @@ def get_term_info(short_form: str):
     :param short_form: short form of the term
     :return: term info
     """
-    pass
+    termInfo = {}
+    vfb_solr = pysolr.Solr('http://query.virtualflybrain.org:8983/solr/vfb_json/', always_commit=True, timeout=990)
+    results = vfb_solr.search('id:' + short_form)
+    vfbTerm = deserialize_term_info(results.docs[0]['term_info'][0])
+    meta = {
+        "Name": "[%s](%s)"%(vfbTerm.term.core.label, vfbTerm.term.core.short_form),
+        }
+    try:
+        meta["Tags"] = vfbTerm.term.core.unique_facets
+    except NameError:
+        meta["Tags"] = vfbTerm.term.core.types
+    try:
+        meta["Description"] = "%s"%("".join(vfbTerm.term.description))
+    except NameError:
+        pass
+    try:
+        meta["Comment"] = "%s"%("".join(vfbTerm.term.comment))
+    except NameError:
+        pass
+    termInfo["meta"] = meta
+    
+    # TODO: Add Queries
+    return termInfo
+

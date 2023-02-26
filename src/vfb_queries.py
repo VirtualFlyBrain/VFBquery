e117c86e2b13594619419330836a4fcad4631ae5
Rob Court <robbie.court@gmail.com>
2023-02-26 10:13:10 +0000

    ensuring results

--- src/vfb_queries.py
+++ src/vfb_queries.py
@@ -14,58 +14,59 @@ def get_term_info(short_form: str):
     """
     termInfo = {}
     results = vfb_solr.search('id:' + short_form)
-    vfbTerm = deserialize_term_info(results.docs[0]['term_info'][0])
-    queries = []
-    meta = {
-        "Name": "[%s](%s)"%(vfbTerm.term.core.label, vfbTerm.term.core.short_form),
-        }
-    meta["SuperTypes"] = vfbTerm.term.core.types
-    try:
-        meta["Tags"] = vfbTerm.term.core.unique_facets
-    except NameError:
-        meta["Tags"] = vfbTerm.term.core.types
-    try:
-        meta["Description"] = "%s"%("".join(vfbTerm.term.description))
-    except NameError:
-        pass
-    try:
-        meta["Comment"] = "%s"%("".join(vfbTerm.term.comment))
-    except NameError:
-        pass
-    termInfo["meta"] = meta
+    if length(results.docs) > 0:
+        vfbTerm = deserialize_term_info(results.docs[0]['term_info'][0])
+        queries = []
+        meta = {
+            "Name": "[%s](%s)"%(vfbTerm.term.core.label, vfbTerm.term.core.short_form),
+            }
+        meta["SuperTypes"] = vfbTerm.term.core.types
+        try:
+            meta["Tags"] = vfbTerm.term.core.unique_facets
+        except NameError:
+            meta["Tags"] = vfbTerm.term.core.types
+        try:
+            meta["Description"] = "%s"%("".join(vfbTerm.term.description))
+        except NameError:
+            pass
+        try:
+            meta["Comment"] = "%s"%("".join(vfbTerm.term.comment))
+        except NameError:
+            pass
+        termInfo["meta"] = meta
 
-    if vfbTerm.anatomy_channel_image and len(vfbTerm.anatomy_channel_image) > 0:
-        images = {}
-        for image in vfbTerm.anatomy_channel_image:
-            label = image.anatomy.label
-            if image.anatomy.symbol != "" and len(image.anatomy.symbol) > 0:
-                label = image.anatomy.symbol
-            if not image.channel_image.image.template_anatomy.short_form in images.keys():
-                images[image.channel_image.image.template_anatomy.short_form]=[]
-            images[image.channel_image.image.template_anatomy.short_form].append({"id":image.anatomy.short_form, "label": label, "thumbnail": image.channel_image.image.image_thumbnail.replace("http://","https://").replace("thumbnailT.png","thumbnail.png")})
-            images[image.channel_image.image.template_anatomy.short_form].append({"id":image.anatomy.short_form, "label": label, "thumbnail_transparent": image.channel_image.image.image_thumbnail.replace("http://","https://").replace("thumbnail.png","thumbnailT.png")})
-            for key in image.channel_image.image.keys():
-                if "images_" in key and not ("thumbnail" in key or "folder" in key):
-                    images[image.channel_image.image.template_anatomy.short_form].append({"id":image.anatomy.short_form, "label": label, key.replace("image_",""): image.channel_image.image[key].replace("http://","https://")})
-        termInfo["Examples"] = images
-        queries.append({"query":"ListAllAvailableImages",label:"List all available images of %s"%(vfbTerm.term.core.label),"function":"get_instances","takes":[{"short_form":{"&&":["Class","Anatomy"]},"default":"%s"%(vfbTerm.term.core.short_form)}]})
-    else:
-        if vfbTerm.channel_image and len(vfbTerm.channel_image) > 0:
+        if vfbTerm.anatomy_channel_image and len(vfbTerm.anatomy_channel_image) > 0:
             images = {}
-            for image in vfbTerm.channel_image:
-                label = vfbTerm.term.core.label
-                if vfbTerm.term.core.symbol != "" and len(vfbTerm.term.core.symbol) > 0:
-                    label = vfbTerm.term.core.symbol
-                if not image.image.template_anatomy.short_form in images.keys():
-                    images[image.image.template_anatomy.short_form]=[]
-                images[image.image.template_anatomy.short_form].append({"id":vfbTerm.term.core.short_form, "label": label, "thumbnail": image.image.image_thumbnail.replace("http://","https://").replace("thumbnailT.png","thumbnail.png")})
-                images[image.image.template_anatomy.short_form].append({"id":vfbTerm.term.core.short_form, "label": label, "thumbnail_transparent": image.image.image_thumbnail.replace("http://","https://").replace("thumbnail.png","thumbnailT.png")})
-                for key in image.image.keys():
+            for image in vfbTerm.anatomy_channel_image:
+                label = image.anatomy.label
+                if image.anatomy.symbol != "" and len(image.anatomy.symbol) > 0:
+                    label = image.anatomy.symbol
+                if not image.channel_image.image.template_anatomy.short_form in images.keys():
+                    images[image.channel_image.image.template_anatomy.short_form]=[]
+                images[image.channel_image.image.template_anatomy.short_form].append({"id":image.anatomy.short_form, "label": label, "thumbnail": image.channel_image.image.image_thumbnail.replace("http://","https://").replace("thumbnailT.png","thumbnail.png")})
+                images[image.channel_image.image.template_anatomy.short_form].append({"id":image.anatomy.short_form, "label": label, "thumbnail_transparent": image.channel_image.image.image_thumbnail.replace("http://","https://").replace("thumbnail.png","thumbnailT.png")})
+                for key in image.channel_image.image.keys():
                     if "images_" in key and not ("thumbnail" in key or "folder" in key):
-                        images[image.image.template_anatomy.short_form].append({"id":vfbTerm.term.core.short_form, "label": label, key.replace("image_",""): image.image[key].replace("http://","https://")})
-            termInfo["Thumbnails"] = images
+                        images[image.channel_image.image.template_anatomy.short_form].append({"id":image.anatomy.short_form, "label": label, key.replace("image_",""): image.channel_image.image[key].replace("http://","https://")})
+            termInfo["Examples"] = images
+            queries.append({"query":"ListAllAvailableImages",label:"List all available images of %s"%(vfbTerm.term.core.label),"function":"get_instances","takes":[{"short_form":{"&&":["Class","Anatomy"]},"default":"%s"%(vfbTerm.term.core.short_form)}]})
+        else:
+            if vfbTerm.channel_image and len(vfbTerm.channel_image) > 0:
+                images = {}
+                for image in vfbTerm.channel_image:
+                    label = vfbTerm.term.core.label
+                    if vfbTerm.term.core.symbol != "" and len(vfbTerm.term.core.symbol) > 0:
+                        label = vfbTerm.term.core.symbol
+                    if not image.image.template_anatomy.short_form in images.keys():
+                        images[image.image.template_anatomy.short_form]=[]
+                    images[image.image.template_anatomy.short_form].append({"id":vfbTerm.term.core.short_form, "label": label, "thumbnail": image.image.image_thumbnail.replace("http://","https://").replace("thumbnailT.png","thumbnail.png")})
+                    images[image.image.template_anatomy.short_form].append({"id":vfbTerm.term.core.short_form, "label": label, "thumbnail_transparent": image.image.image_thumbnail.replace("http://","https://").replace("thumbnail.png","thumbnailT.png")})
+                    for key in image.image.keys():
+                        if "images_" in key and not ("thumbnail" in key or "folder" in key):
+                            images[image.image.template_anatomy.short_form].append({"id":vfbTerm.term.core.short_form, "label": label, key.replace("image_",""): image.image[key].replace("http://","https://")})
+                termInfo["Thumbnails"] = images
 
-    termInfo["Queries"] = queries
+        termInfo["Queries"] = queries
     return termInfo
 
 def get_instances(short_form: str):

67b8e24d745d157bfae9844db681e94e0cd737c8
Rob Court <robbie.court@gmail.com>
2023-03-10 14:01:07 +0000

    relative import with no known parent package

--- src/vfb_queries.py
+++ src/vfb_queries.py
@@ -1,5 +1,5 @@
 import pysolr
-from .term_info_queries import deserialize_term_info
+from term_info_queries import deserialize_term_info
 from vfb_connect.cross_server_tools import VfbConnect
 from marshmallow import Schema, fields
 

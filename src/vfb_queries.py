fded572a32bb7a4aecb954e6afe85840e6d757ed
Rob Court <robbie.court@gmail.com>
2023-03-12 10:23:17 +0000

    updating schema

--- src/vfb_queries.py
+++ src/vfb_queries.py
@@ -21,8 +21,7 @@ class QuerySchema(Schema):
     query = fields.String(required=True)
     label = fields.String(required=True)
     function = fields.String(required=True)
-    takes = fields.String(required=True)
-    default = fields.String(required=False)
+    takes = fields.Dict(keys=fields.String(), values=fields.Dict(), required=True)
 
 class Coordinates:
     def __init__(self, X, Y, Z):

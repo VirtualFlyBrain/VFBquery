f9fc83bcec69fefb3b8fbaa64b9fee5845426aac
Rob Court <robbie.court@gmail.com>
2023-03-14 14:47:34 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,12 +15,12 @@ with open(path.join(here, 'README.md')) as f:
     long_description = f.read()
 
 setup(
-    name="vfb_queries",
+    name="vfbquery",
     version=__version__,
     description="Wrapper for querying VirtualFlyBrain knowledge graph.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/VirtualFlyBrain/VFB_queries",
+    url="https://github.com/VirtualFlyBrain/VFBquery",
     author="VirtualFlyBrain",
     license="GPL-3.0 License",
     classifiers=[
@@ -34,6 +34,6 @@ setup(
     install_requires=["vfb_connect", "dataclasses-json", "dacite", "requests", "pysolr"],
     project_urls={  # Optional
         'Bug Reports': 'https://github.com/VirtualFlyBrain/VFB_queries/issues',
-        'Source': 'https://github.com/VirtualFlyBrain/VFB_queries'
+        'Source': 'https://github.com/VirtualFlyBrain/VFBquery'
     },
 )

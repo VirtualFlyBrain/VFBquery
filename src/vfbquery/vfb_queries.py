6abbf254236a6f240922205813097ffa780c3e31
Rob Court <robbie.court@gmail.com>
2023-05-06 09:53:47 +0100

    enabling query schema

--- src/vfbquery/vfb_queries.py
+++ src/vfbquery/vfb_queries.py
@@ -159,7 +159,7 @@ class TermInfoOutputSchema(Schema):
     SuperTypes = fields.List(fields.String(), required=True)
     Meta = fields.Dict(keys=fields.String(), values=fields.String(), required=True)
     Tags = fields.List(fields.String(), required=True)
-    Queries = fields.Raw(required=False) #having issues to serialize
+    Queries = fields.List(QueryField(), required=False)
     IsIndividual = fields.Bool(missing=False, required=False)
     Images = fields.Dict(keys=fields.String(), values=fields.List(fields.Nested(ImageSchema()), missing={}), required=False, allow_none=True)
     IsClass = fields.Bool(missing=False, required=False)

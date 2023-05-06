e330e25da25777fd4bea073c36261ee52dc08b71
Rob Court <robbie.court@gmail.com>
2023-05-06 12:26:32 +0100

    fix query schema

--- src/vfbquery/vfb_queries.py
+++ src/vfbquery/vfb_queries.py
@@ -163,7 +163,7 @@ class TermInfoOutputSchema(Schema):
     SuperTypes = fields.List(fields.String(), required=True)
     Meta = fields.Dict(keys=fields.String(), values=fields.String(), required=True)
     Tags = fields.List(fields.String(), required=True)
-    Queries = fields.List(QueryField(), required=False)
+    Queries = fields.Nested(QuerySchema(), many=True, required=False)
     IsIndividual = fields.Bool(missing=False, required=False)
     Images = fields.Dict(keys=fields.String(), values=fields.List(fields.Nested(ImageSchema()), missing={}), required=False, allow_none=True)
     IsClass = fields.Bool(missing=False, required=False)

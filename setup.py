5d4a465284cf9786c19a78f99f57850e6fef0887
Rob Court <robbie.court@gmail.com>
2026-03-19 08:14:00 +0000

    Bump version to 1.7.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-__version__ = "1.7.1"
+__version__ = "1.7.2"
 
 # Get the long description from the README file
 with open(path.join(here, 'README.md')) as f:
